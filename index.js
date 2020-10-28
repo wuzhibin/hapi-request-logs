dd45bf6d2fa8023e625754ee5c77bb30a7523103
wuzhibin <code@wuzhibin.me>
2020-10-29 00:22:29 +0800

    supoort ua

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Redis = require('ioredis');
 const Base64 = require('js-base64')
+const parser = require('ua-parser-js')
+const util = require('util')
 
 async function register(server, options) {
     if (!Object.keys(options).length)
@@ -34,6 +36,7 @@ async function register(server, options) {
 
             let info = {}
             let timestamp = new Date().getTime()
+            let ua = parser(request.headers['user-agent'])
             info.remote = request.headers.x_forwarded_for || request.headers['x-real-ip'] || request.headers['x-forwarded-for'] || request.info.remoteAddress
             info.host = request.headers.host
             info.path = request.path
@@ -42,6 +45,9 @@ async function register(server, options) {
             info.referer = request.headers.referer
             info.created_at = request.info.received || timestamp
             info.token = request.headers.authorization != undefined && request.headers.authorization != "undefined" ? request.headers.authorization : ""
+            info.app_version = util.format('%s %s', ua.browser.name, ua.browser.version)
+            info.system = util.format('%s %s', ua.os.name, ua.os.version)
+            info.brand_model = util.format('%s %s', ua.device.vendor || 'PC', ua.device.model || '')
 
             try {
                 if (info.token.indexOf('.') > -1) {
